c57f992bb8cc101d3dd9bb8f54b1cd1d8ac310dc
Nisanth Nadimpalli <70921921+Axtro-PhysX@users.noreply.github.com>
2022-04-27 16:05:06 -0400

    Create level1.sh

---
+++ level1.sh
@@ -0,0 +1,22 @@
+  For more information regarding individual wargames, visit
+  http://www.overthewire.org/wargames/
+
+  For support, questions or comments, contact us through IRC on
+  irc.overthewire.org #wargames.
+
+  Enjoy your stay!
+
+bandit1@bandit:~$ man ls
+bandit1@bandit:~$ ls -alps
+total 24
+4 -rw-r-----  1 bandit2 bandit1   33 May  7  2020 -
+4 drwxr-xr-x  2 root    root    4096 May  7  2020 ./
+4 drwxr-xr-x 41 root    root    4096 May  7  2020 ../
+4 -rw-r--r--  1 root    root     220 May 15  2017 .bash_logout
+4 -rw-r--r--  1 root    root    3526 May 15  2017 .bashrc
+4 -rw-r--r--  1 root    root     675 May 15  2017 .profile
+bandit1@bandit:~$ cat ./-
+CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
+bandit1@bandit:~$ exit
+logout
+Connection to bandit.labs.overthewire.org closed.
